8f50b30a6fd935d57a0d8bdb92310e04b9bf9867
Christian Eisele <3015571@stud.hs-mannheim.de>
2025-06-19 15:18:09 +0200

    New Features in ChessEngine added

--- src/main/java/de/thm/informatik/chess/domain/ChessEngine.java
+++ src/main/java/de/thm/informatik/chess/domain/ChessEngine.java
@@ -2,6 +2,7 @@ package de.thm.informatik.chess.domain;
 
 import com.github.bhlangonijr.chesslib.Board;
 import com.github.bhlangonijr.chesslib.Piece;
+import com.github.bhlangonijr.chesslib.Side;
 import com.github.bhlangonijr.chesslib.move.Move;
 import com.github.bhlangonijr.chesslib.Square;
 import java.util.List;
@@ -33,6 +34,25 @@ public class ChessEngine {
     public boolean isGameOver() {
 
         return board.isMated() || board.isDraw();
+    }
+    public boolean isCheckmate() {
+        return board.isMated();
+    }
+
+    public boolean isInCheck() {
+        return board.isKingAttacked();
+    }
+
+    public boolean isStalemate() {
+        return board.isStaleMate();
+    }
+
+    public String getWinner() {
+        if (isCheckmate()) {
+            Side loser = board.getSideToMove(); // der am Zug ist, ist matt
+            return loser == Side.WHITE ? "Schwarz" : "Weiß";
+        }
+        return null;
     }
 
     public void reset() {
