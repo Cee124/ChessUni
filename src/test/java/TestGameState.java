f2e238280c5fef053b3b870514ebd6b55121d765
3015972 <3015972@stud.hs-mannheim.de>
2025-06-22 21:39:40 +0200

    minor import change

--- src/test/java/TestGameState.java
+++ src/test/java/TestGameState.java
@@ -1,11 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
